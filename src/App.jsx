3df92415fe831cd4c9ec3b7920d4a324170a5943
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-28 22:03:13 +0530

    structure completed 7:00:22

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
   Stats,
   Business,
   Billing,
-  cardDeal,
+  CardDeal,
   Testimonials,
   Clients,
   CTA,
@@ -15,15 +15,20 @@ const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business Billing cardDeal Testimonials Clients CTA Footer
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
+          <Clients /> <CTA /> <Footer />
         </div>
       </div>
     </div>
