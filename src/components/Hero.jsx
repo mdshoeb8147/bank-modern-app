9963276d3c485224f67d717106c80887f52ca777
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-29 21:02:54 +0530

    started with hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import styles from "../style";
+import { discount, robot } from "../assets";
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className=""></div>
+  </section>
+);
 export default Hero;
